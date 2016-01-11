89e2034803bd2199901af960b9532d9b579e20a9
Stephen Zhao <mail@zhaostephen.com>
2016-01-11 14:00:14 -0500

    updated resume

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app){
 
 	app.get('/downloads/Resume', function(req, res){
 		console.log('sending resume...');
-		res.sendFile(path.resolve(__dirname + '/../res/StephenZhao_Resume.pdf'));
+		res.sendFile(path.resolve(__dirname + '/../res/StephenZhao_RE_v201601090059_new-me.pdf'));
 	});
 
 	app.get('/api/profile', function(req, res) {
